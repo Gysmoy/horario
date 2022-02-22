2b8de96fdef45e464441cf0bb5c1b9fe8633a5fb
Gysmoy <gamboapalominocarlosmanuel@gmail.com>
2022-02-22 14:16:44 -0500

    NTS

--- assets/js/schedule.js
+++ assets/js/schedule.js
@@ -1,3 +1,37 @@
-$(document).on('click', '#schedule', function () {
-    $('#schedule-table').modal('open');
-})
+$(document).on("click", "#schedule", function () {
+  var modal = $("#schedule-table");
+  modal.empty();
+  modal.modal("open");
+  for (let i = 0; i < 7; i++) {
+    var day = horario[`day${i}`];
+    if (day) {
+      var template = '';
+      template += `
+        <div>
+          <span>
+              ${days[i]}
+          </span>
+          <div>
+      `;
+      day.forEach(d => {
+        template += `
+          <div>${d.course}</div>
+        `;
+      });
+      template += `    
+          </div>
+        </div>
+      `;
+      modal.append(template)
+    } else {
+      modal.append(`
+        <div>
+          <span>
+              ${days[i]}
+          </span>
+          <div>- No hay clases en este día -</div>
+        </div>
+      `);
+    }
+  }
+});
--- assets/js/vars.js
+++ assets/js/vars.js
@@ -1,26 +1,38 @@
+var days = {
+    '0': 'D',
+    '1': 'L',
+    '2': 'M',
+    '3': 'X',
+    '4': 'J',
+    '5': 'V',
+    '6': 'S'
+};
 var courses = {
     "FPR": "FORMACIÓN PRÁCTICA REMOTA",
     "FME": "FORMACIÓN DE MONITORES DE EMPRESA",
-    "SYP": "SOFTWARE Y PROTOTIPADO"
+    "SYP": "SOFTWARE Y PROTOTIPADO",
+    "SIE": "SOFTWARE INTEGRATION EIRL"
 };
 var links = {
     "FPR": "https://teams.microsoft.com/l/meetup-join/19%3ameeting_Y2I5ZjdiZTUtZDU2Yy00MjAxLWJkOWEtYWYxZTk5ODYxZGEy%40thread.v2/0?context=%7b%22Tid%22%3a%22b4a40545-7779-4b38-aff7-1f1738f80840%22%2c%22Oid%22%3a%2262bb5d48-3c99-467a-9a06-047dca328b0d%22%7d",
     "FME": "https://teams.microsoft.com/l/meetup-join/19%3ameeting_YTNmODEyYWEtMTQ3OC00YzAyLWE0MDgtNjNhMTk5Y2MxMzNk%40thread.v2/0?context=%7b%22Tid%22%3a%22b4a40545-7779-4b38-aff7-1f1738f80840%22%2c%22Oid%22%3a%22d499d961-a0c5-41ed-bb9a-cfb5181f9b34%22%7d",
-    "SYP": "https://teams.microsoft.com/l/meetup-join/19%3ameeting_ZWQyN2ViZGYtODYxYi00MTdhLTg5ODEtNDgzZmQwMjgwZjBl%40thread.v2/0?context=%7b%22Tid%22%3a%22b4a40545-7779-4b38-aff7-1f1738f80840%22%2c%22Oid%22%3a%22a26ca2dd-e541-4d46-aa85-d90977bad110%22%7d"
+    "SYP": "https://teams.microsoft.com/l/meetup-join/19%3ameeting_ZWQyN2ViZGYtODYxYi00MTdhLTg5ODEtNDgzZmQwMjgwZjBl%40thread.v2/0?context=%7b%22Tid%22%3a%22b4a40545-7779-4b38-aff7-1f1738f80840%22%2c%22Oid%22%3a%22a26ca2dd-e541-4d46-aa85-d90977bad110%22%7d",
+    "SIE": "https://wa.me/?text=Buenos días ingeniero, buenos días a todos: https://meet.google.com/bxt-oavn-uxi"
 };
 var instructors = {
     "PNJA": "POMA NUÑEZ, JOSÉ ANTONIO",
     "DCLM": "DE LA CRUZ CAMPOS, LEANDRO MARCELINO",
-    "VDJC": "VILCHEZ DEGREGORI, JUAN CARLOS"
+    "VDJC": "VILCHEZ DEGREGORI, JUAN CARLOS",
+    "FB": "FREDDY BENDEZÚ"
 };
 var horario = {
     "day1": [
         {
             "start": "08:00",
             "end": "13:00",
-            "course": "KARLOS SEGUROS",
-            "instructor": "FREDDY BENDEZÚ",
-            "link": "https://wa.me/?text=Buenos días ingeniero, buenos días a todos: https://meet.google.com/bxt-oavn-uxi",
+            "course": courses.SIE,
+            "instructor": instructors.FB,
+            "link": links.SIE
         },
         {
             "start": "13:30",
@@ -41,9 +53,10 @@ var horario = {
         {
             "start": "08:00",
             "end": "13:00",
-            "course": "KARLOS SEGUROS",
-            "instructor": "FREDDY BENDEZÚ",
-            "link": "https://wa.me/?text=Buenos días ingeniero, buenos días a todos: https://meet.google.com/bxt-oavn-uxi",
+            "course": courses.SIE,
+            "instructor": instructors.FB,
+            "link": links.SIE,
+            "message": "Queda 30 minutos, termina ya los requerimientos",
         },
         {
             "start": "13:30",
@@ -64,9 +77,9 @@ var horario = {
         {
             "start": "08:00",
             "end": "13:00",
-            "course": "KARLOS SEGUROS",
-            "instructor": "FREDDY BENDEZÚ",
-            "link": "https://wa.me/?text=Buenos días ingeniero, buenos días a todos: https://meet.google.com/bxt-oavn-uxi",
+            "course": courses.SIE,
+            "instructor": instructors.FB,
+            "link": links.SIE
         },
         {
             "start": "13:30",
@@ -87,9 +100,9 @@ var horario = {
         {
             "start": "08:00",
             "end": "13:00",
-            "course": "KARLOS SEGUROS",
-            "instructor": "FREDDY BENDEZÚ",
-            "link": "https://wa.me/?text=Buenos días ingeniero, buenos días a todos: https://meet.google.com/bxt-oavn-uxi",
+            "course": courses.SIE,
+            "instructor": instructors.FB,
+            "link": links.SIE
         },
         {
             "start": "13:30",
