e0464838f45855a3b4edb93b50f2584f2f48120e
Gysmoy <gamboapalominocarlosmanuel@gmail.com>
2022-04-14 22:04:30 -0500

    gambo

--- assets/js/download/main.js
+++ assets/js/download/main.js
@@ -13,17 +13,23 @@ function saveFile(data, filename) {
     }
 }
 
-$(document).on('click', '#download', function() {
-    var attendants = $('#check_attendants').is(':checked') ?
-        JSON.parse(localStorage.getItem('attendants')) : null;
-    var subjects = $('#check_subjects').is(':checked') ?
-        JSON.parse(localStorage.getItem('subjects')) : null;
-    var events = $('#check_events').is(':checked') ?
-        JSON.parse(localStorage.getItem('events')) : null;
-    var data = {
-        attendants: attendants,
-        subjects: subjects,
-        events: events
-    };
-    saveFile(JSON.stringify(data), 'backup.json');
+$(document).on('click', '#download', function () {
+    var checked = $('#form-download input:checked').length;
+    if (checked == 0) {
+        alert('Debe seleccionar al menos un elemento');
+        return;
+    } else {
+        var attendants = $('#check_attendants').is(':checked') ?
+            JSON.parse(localStorage.getItem('attendants')) : null;
+        var subjects = $('#check_subjects').is(':checked') ?
+            JSON.parse(localStorage.getItem('subjects')) : null;
+        var events = $('#check_events').is(':checked') ?
+            JSON.parse(localStorage.getItem('events')) : null;
+        var data = {
+            attendants: attendants,
+            subjects: subjects,
+            events: events
+        };
+        saveFile(JSON.stringify(data), 'backup.json');
+    }
 })
