0548dc572083215a9dda8a69ccc03320c6f527f5
Gysmoy <gamboapalominocarlosmanuel@gmail.com>
2022-04-17 17:18:13 -0500

    gambo

--- assets/js/general/consts.js
+++ assets/js/general/consts.js
@@ -1,3 +1,14 @@
 const days = ['D', 'L', 'M', 'X', 'J', 'V', 'S'];
 const days_label = ['DOMINGO', 'LUNES', 'MARTES', 'MIÉRCOLES', 'JUEVES', 'VIERNES', 'SÁBADO'];
 moment.locale('es');
+
+// agregar events, attendants, subjects si no existen en localstorage
+if (!localStorage.getItem('events')) {
+    localStorage.setItem('events', JSON.stringify({}));
+}
+if (!localStorage.getItem('attendants')) {
+    localStorage.setItem('attendants', JSON.stringify({}));
+}
+if (!localStorage.getItem('subjects')) {
+    localStorage.setItem('subjects', JSON.stringify({}));
+}
